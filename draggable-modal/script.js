b87660e144935567021d24b2cb02aaccce0511ec
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2021-02-28 01:42:27 +0900

    最初にエリア取得しておく

--- draggable-modal/script.js
+++ draggable-modal/script.js
@@ -1,9 +1,10 @@
 (function () {
   function toDraggable(element) {
+    const draggableAreaElement = element.closest(".js-draggable-area");
     let holding = false;
     let offsetX = -1;
     let offsetY = -1;
-    
+
     element.addEventListener("mousedown", (ev) => {
       holding = true;
       offsetX = ev.offsetX;
@@ -15,12 +16,9 @@
         return;
       }
 
-      const contentElement = element.closest(".js-draggable-area");
-      if (contentElement) {
-        contentElement.style.position = "absolute";
-        contentElement.style.left = ev.pageX - offsetX + "px";
-        contentElement.style.top = ev.pageY - offsetY + "px";
-      }
+      draggableAreaElement.style.position = "absolute";
+      draggableAreaElement.style.left = ev.pageX - offsetX + "px";
+      draggableAreaElement.style.top = ev.pageY - offsetY + "px";
     });
     document.addEventListener("mouseup", (ev) => {
       holding = false;
