828c535f58c9ec8b18dfd6e09ad86468638c2a22
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2025-07-28 01:58:19 +0900

    エントリポイントをかき集め

--- await-semaphore-sandbox/vite.config.js
+++ await-semaphore-sandbox/vite.config.js
@@ -1,9 +1,12 @@
 import { dirname, resolve } from "node:path";
+import { readdirSync, existsSync } from "node:fs";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const srcDir = resolve(__dirname, "src")
+
+const entryPoints = collectEntryPoints()
 
 export default defineConfig({
   root: srcDir,
@@ -20,7 +23,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: resolve(srcDir, "index.html"),
-        sandbox01: resolve(srcDir, "sandbox01/index.html"),
+        ...entryPoints
       },
     },
   },
@@ -28,3 +31,27 @@ export default defineConfig({
     "process.env": {}
   }
 });
+
+function collectEntryPoints() {
+  // src直下のファイル/ディレクトリ一覧
+  const dirents = readdirSync(srcDir, { withFileTypes: true });
+
+  const entryPoints = dirents
+    // index.htmlを含むものを列挙
+    .filter((dirent) => {
+      if (dirent.isFile()) {
+        return false;
+      }
+
+      const index = resolve(dirent.parentPath, dirent.name, "index.html");
+      return existsSync(index);
+    })
+    // rollupOptions用に エントリ名(フォルダ名)をキーに,index.htmlのパスを値にするオブジェクトを作成
+    .reduce((prev, current) => {
+
+      prev[current.name] = resolve(current.parentPath, current.name, "index.html")
+      return prev
+    }, {})
+
+  return entryPoints;
+}
